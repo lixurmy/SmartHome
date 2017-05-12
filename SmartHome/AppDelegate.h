270e73a985d06ad94ff21ade610f0273931f6663
Xu Li <xuli@XudeMacBook-Pro.local>
2017-05-12 21:34:22 +0800

    Initial Commit

---
+++ SmartHome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartHome
+//
+//  Created by Xu Li on 2017/5/12.
+//  Copyright © 2017年 Xu Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartHome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartHome
+//
+//  Created by Xu Li on 2017/5/12.
+//  Copyright © 2017年 Xu Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
